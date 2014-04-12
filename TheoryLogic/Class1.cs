937436779d31f51fe550e04105b071f573502d8c
tamirdennis <tamirdennis@gmail.com>
2014-04-12 16:30:11 +0300

    Added TheoryLogic

---
+++ TheoryLogic/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheoryLogic
+{
+    public interface ICar
+    {
+        enum TurnDirection
+        {
+            Right,
+            Left
+        }
+        void ChangeSpeed(double speedChanged);
+        void Turn(TurnDirection direction);
+        void ChangeLane(TurnDirection dirction);
+    }
+    public class Car
+    {
+        public 
+    }
+}
